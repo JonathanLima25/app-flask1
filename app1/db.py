4a0f426cb743f137b4d7035a2cfc30db423f080b
Jonathan Lima <38431639+JonathanLima25@users.noreply.github.com>
2020-03-05 16:27:13 -0300

    Update db.py

--- app1/db.py
+++ app1/db.py
@@ -1,11 +1,11 @@
 users = {
-    "michael": {
-        "name": "Michael Scott",
+    "jonathan": {
+        "name": "Jonathan Noj",
         "image": "https://api.adorable.io/avatars/100/michael.png",
         "tel": "1489-7895"
     },
-    "david": {
-        "name": "David Brent",
+    "leonardo": {
+        "name": "Leonardo Silva",
         "image": "https://api.adorable.io/avatars/100/david.png",
         "tel": "5555-5555"
     }
